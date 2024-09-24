6e38e8362c1c0c06ffd21c7e06e8085a285148af
DialloMaria <diallomariam0715@gmail.com>
2024-09-24 12:19:26 +0000

    creations des routes ainsi que l'ajout de certain components

---
+++ src/app/Components/portails/portails.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PortailsPipe } from './portails.pipe';
+
+describe('PortailsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PortailsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { InscriptionComponent } from './Components/Authentifications/Inscriptions/inscription.component';
+import { ConnexionComponent } from './Components/Authentifications/Connexions/connexion.component';
+import { DonComponent } from './Components/Donateurs/Dons/don.component';
+import { AccueilComponent } from './Components/Portail/accueil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+
+// Routes portails et par defaut
+  {path:'**', pathMatch:'full', redirectTo:'accueil'},
+  {path: 'accueil', component: AccueilComponent},
+
+// Routes pour l'authentification
+  {path: 'inscription', component:InscriptionComponent},
+  {path: 'conxeion', component:ConnexionComponent},
+
+
+  // Route pour les Donateurs
+  {path: 'donateur' , component:DonComponent},
+
+
+
+  // Route pour les Organisations
+
+
+
+  // Route pour les Beneficiaires
+
+
+
+
+];
+
